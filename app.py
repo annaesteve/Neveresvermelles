6a2f8b461532f368ce044d9dba662cbac5c5b1c2
jordipalomeraa <jordipalomera21@gmail.com>
2023-05-13 14:14:13 +0200

    Descarregar fotos amb link públic

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import os
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
 from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 
@@ -12,9 +9,17 @@ def index():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
-            uploaded_file.save(uploaded_file.filename)
+            p = os.path.join('uploads', uploaded_file.filename)
+            #uploaded_file.save(uploaded_file.filename)
+            uploaded_file.save(p)
         return redirect(url_for('index'))
     return render_template('index.html')
+
+#url_for('download_file', id=nom)
+@app.route('/get_image/<id>')
+def download_file(id):
+    p = os.path.join('uploads', id)
+    return send_file(p, as_attachment=True)
 
 if __name__ == '__main__':
     app.run()
