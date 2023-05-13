748f69fd761aad1ae0756df50127e7405fed6640
jordipalomeraa <jordipalomera21@gmail.com>
2023-05-13 12:53:58 +0200

    Penjar fitxer al servidor

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "principal"
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
-@app.route('/foto')
-def foto():
-    img = os.path.join('static', 'Image')
-    file = os.path.join(img, 'imatge.png')
-    return render_template('img_render.html', image=file)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        uploaded_file = request.files['file']
+        if uploaded_file.filename != '':
+            uploaded_file.save(uploaded_file.filename)
+        return redirect(url_for('index'))
+    return render_template('img_render.html')
