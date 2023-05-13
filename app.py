d998d6d6f5e6b11297ada6f7f18c62d232e550e7
jordipalomeraa <jordipalomera21@gmail.com>
2023-05-13 13:29:11 +0200

    Penjar fitxers bé

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
-def upload_file():
-    uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        uploaded_file.save(uploaded_file.filename)
-    return redirect(url_for('index'))
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,4 +14,7 @@ def index():
         if uploaded_file.filename != '':
             uploaded_file.save(uploaded_file.filename)
         return redirect(url_for('index'))
-    return render_template('img_render.html')
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
