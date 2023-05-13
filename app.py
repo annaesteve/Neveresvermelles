ddb94de6afd713b97cb1b7b92fa21cab18844d16
jordipalomeraa <jordipalomera21@gmail.com>
2023-05-13 12:18:03 +0200

    a

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('img_render.html')
+    return "principal"
+
+@app.route('/foto')
+def foto():
+    img = os.path.join('static', 'Image')
+    file = os.path.join(img, 'imatge.png')
+    return render_template('img_render.html', image=file)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
