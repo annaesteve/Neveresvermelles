6ed94082689e233d8ca3bd8e95f80a35e09f9560
jordipalomeraa <jordipalomera21@gmail.com>
2023-05-14 03:14:32 +0200

    coses

--- app.py
+++ app.py
@@ -14,24 +14,32 @@ app = Flask(__name__)
 #Pàgina inicial
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    print("index")
     if request.method == 'POST':
-        if 'Kitchen' in request.form:
-            return redirect(url_for('kitchen'))
-        
-        elif 'Bathroom' in request.form:
-            return redirect(url_for('bathroom'))
-
+        uploaded_file = request.files['file']
+        t = request.form['prova']
+        print(t)
+        if uploaded_file.filename != '':
+            p = os.path.join('static', 'Images', uploaded_file.filename)            
+            uploaded_file.save(p)
+        #return redirect(url_for('index'))
+        return redirect(url_for('send', imatge=uploaded_file.filename)) 
     return render_template('index.html')
+
+       
+
 
 #Retorna una imatge guardada
 @app.route('/get/<id>')
 def download_file(id):
+    print("get")
     p = os.path.join('static', 'Images', id)
     return send_file(p, as_attachment=False)
 
 #Envia una imatge a restb.ai
 @app.route('/send/<imatge>')
 def send(imatge):
+    print("send")
     p = os.path.join('static', 'Images', imatge)
     url_final = request.base_url.replace('send', 'get') 
 
@@ -49,82 +57,5 @@ def send(imatge):
 
     return render_template('result.html', resultat=json_response)
 
-#Entra a les opcions de la cuina
-@app.route('/kitchen', methods=['GET', 'POST'])
-def kitchen():
-    if request.method == 'POST':
-        if 'Fridge' in request.form:
-            print('Fridge', file=sys.stderr)
-            return redirect(url_for('fridge'))
-        
-        elif 'Stove' in request.form:
-            return redirect(url_for('stove'))
-    
-    return render_template('kitchen.html')
-
-@app.route('/fridge', methods=['GET', 'POST'])
-def fridge():
-    if request.method == 'POST':
-        if 'file' in request.form:
-            uploaded_file = request.files['file']
-            if uploaded_file.filename != '':
-                p = os.path.join('uploads', uploaded_file.filename)
-                uploaded_file.save(uploaded_file.filename)
-                uploaded_file.save(p)
-            return redirect(url_for('fridge'))
-
-    return render_template('fridge.html')
-
-@app.route('/stove', methods=['GET', 'POST'])
-def stove():
-    if request.method == 'POST':
-        if 'file' in request.form:
-            uploaded_file = request.files['file']
-            if uploaded_file.filename != '':
-                p = os.path.join('uploads', uploaded_file.filename)
-                uploaded_file.save(uploaded_file.filename)
-                uploaded_file.save(p)
-            return redirect(url_for('stove'))
-
-    return render_template('stove.html')
-
-#Entra a les opcions del bany
-@app.route('/bathroom', methods=['GET', 'POST'])
-def bathroom():
-    if request.method == 'POST':
-        if 'Toilet' in request.form:
-            return redirect(url_for('toilet'))
-        
-        elif 'Sink' in request.form:
-            return redirect(url_for('sink'))
-    
-    return render_template('bathroom.html')
-
-@app.route('/toilet', methods=['GET', 'POST'])
-def toilet():
-    if request.method == 'POST':
-        if 'file' in request.form:
-            uploaded_file = request.files['file']
-            if uploaded_file.filename != '':
-                p = os.path.join('uploads', uploaded_file.filename)
-                uploaded_file.save(uploaded_file.filename)
-                uploaded_file.save(p)
-            return redirect(url_for('toilet'))
-
-    return render_template('toilet.html')
-
-@app.route('/sink', methods=['GET', 'POST'])
-def sink():
-    if request.method == 'POST':
-        if 'file' in request.form:
-            uploaded_file = request.files['file']
-            if uploaded_file.filename != '':
-                p = os.path.join('uploads', uploaded_file.filename)
-                uploaded_file.save(uploaded_file.filename)
-                uploaded_file.save(p)
-            return redirect(url_for('sink'))
-
-    return render_template('sink.html')
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
